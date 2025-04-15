0c29853219dc6daa3d2f46978a133c6c31fab0c4
Vipin <vipincvmavila@gmail.com>
2025-04-15 19:42:05 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
+import users from '../views/users.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
   { path: '/contact', name: 'Contact', component: Contact },
+    { path: '/users', name: 'users', component: users },
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
