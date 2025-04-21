2a5c151142c7e993a44beb1fdb750dfcf6d69797
Vipin <vipincvmavila@gmail.com>
2025-04-21 14:12:51 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,16 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
 import users from '../views/users.vue'
+import Product from '../views/Product.vue'
+import Purchase from '../views/Purchase.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
   { path: '/contact', name: 'Contact', component: Contact },
-    { path: '/users', name: 'users', component: users },
+  { path: '/users', name: 'users', component: users },
+  { path: '/product', name: 'Product', component: Product },
+  { path: '/purchase', name: 'Purchase', component: Purchase },
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
