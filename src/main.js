91dcdedab8b842110d33698e16e9f60bf798715b
Vipin <vipincvmavila@gmail.com>
2025-04-23 23:07:23 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+const pinia = createPinia()
 
-createApp(App).use(router).use(createPinia).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
