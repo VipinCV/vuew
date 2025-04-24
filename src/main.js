858e36c303e640d9ad092978853af61c28d205d6
Vipin <vipincvmavila@gmail.com>
2025-04-24 20:37:59 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,16 +4,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import axios from 'axios';
-const pinia = createPinia();
-
-import { useAuthStore } from './stores/auth';
-axios.interceptors.request.use(config => {
-  const authStore = useAuthStore();
-  if (authStore.token) {
-    config.headers.Authorization = `Bearer ${authStore.token}`;
-  }
-  return config;
-});
+const pinia = createPinia(); 
 
 
 createApp(App).use(router).use(pinia).mount('#app')
